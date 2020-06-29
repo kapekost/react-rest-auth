f72f46918edb5131bac559cdd36407b5b1ad73e6
kapekost <kapekost@gmail.com>
2020-06-30 00:32:09 +0100

    add mongo container and storage

--- server/src/storage.js
+++ server/src/storage.js
@@ -1,8 +1,17 @@
 const moment = require("moment");
-
+const { MongoClient } = require("mongodb");
 module.exports = class Storage {
   constructor() {
-    this.config = {};
+    const { MONGO_HOST, MONGO_PORT, MONGO_INITDB_DATABASE } = process.env;
+    this.config = {
+      MONGO_INITDB_DATABASE,
+      MONGO_URI: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_INITDB_DATABASE}?retryWrites=true&w=majority`,
+    };
+    this.init().then((client) => {
+      this.mongoClient = client;
+      // Make the appropriate DB calls
+      this.database = client.db(this.config.MONGO_INITDB_DATABASE);
+    });
   }
 
   getDocument(data) {
@@ -12,5 +21,22 @@ module.exports = class Storage {
   getUser(data) {
     console.log(data);
     return { user: "username" };
+  }
+
+  async init() {
+    console.log(this.config);
+    const client = new MongoClient(this.config.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+      console.log("mongo client connected");
+      return client;
+    } catch (e) {
+      console.error(e);
+    }
   }
 };
